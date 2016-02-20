e8e591fd4716595480330428c2c3e2e5e403cbda
Patrick Graham <Pabreetzio@gmail.com>
2016-02-19 21:32:31 -0500

    Initial Commit

---
+++ Scale/Scale.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AviaSemiconductor;
+using Windows.Devices.Gpio;
+
+namespace Scale
+{
+    public sealed class Scale
+    {
+        private int offset;
+        private double calibrationConstant;
+        private HX711 device; 
+        public Scale()
+        {
+            GpioController controller = GpioController.GetDefault();
+            device = new HX711();
+            device.PowerDownAndSerialClockInput = controller.OpenPin(14);
+            device.SerialDataOutput = controller.OpenPin(15);
+            offset = 0;
+            calibrationConstant = 1;
+        }
+        public double GetReading()
+        {
+            return (device.Read() - offset) * calibrationConstant;
+        }
+        public void Tare()
+        {
+            offset = device.Read();
+        }
+        public void Calibrate(int grams)
+        {
+            calibrationConstant = (device.Read() - offset) / grams;
+        }
+        
+    }
+}
