ecb9d8e69904a7a4fd7c58d3c80c93ef52c36bde
Patrick Graham <Pabreetzio@gmail.com>
2016-02-23 02:04:49 -0500

    Finally getting readings from the scale! Not sure if they're representative of weight or not. Usually they seem to be, but every once in a while the number is way out there.

--- Components/Scale.cs
+++ Components/Scale.cs
@@ -8,9 +8,10 @@ namespace Components
         private int offset;
         private double calibrationConstant;
         private HX711 device; 
+        GpioPin dataPin;
+        GpioPin clockPin;
         public Scale()
         {
-
             offset = 0;
             calibrationConstant = 1;
         }
@@ -20,24 +21,43 @@ namespace Components
             {
                 GpioController controller = GpioController.GetDefault();
                 GpioOpenStatus status;
-                GpioPin dt;
-                GpioPin sck;
                 if (controller != null
-                    && controller.TryOpenPin(23, GpioSharingMode.Exclusive, out sck, out status)
-                    && controller.TryOpenPin(24, GpioSharingMode.Exclusive, out dt, out status))
+                    && controller.TryOpenPin(23, GpioSharingMode.Exclusive, out clockPin, out status)
+                    && controller.TryOpenPin(24, GpioSharingMode.Exclusive, out dataPin, out status))
                 {
-                    device = new HX711(sck, dt);
+                    clockPin.SetDriveMode(GpioPinDriveMode.Output);
+                    dataPin.SetDriveMode(GpioPinDriveMode.Input);
+                    device = new HX711(clockPin, dataPin);
                     device.PowerOn();
                 }
                 else device = null;
             }
         }
+        private void releaseDevice()
+        {
+            GpioController controller = GpioController.GetDefault();
+            device.PowerDown();
+            dataPin.Dispose();
+            clockPin.Dispose();
+        }
+        int BoolArrayToInt(bool[] bits)
+        {
+            if (bits.Length > 32) throw new System.Exception("Can only fit 32 bits in a int");
+
+            int r = 0;
+            for (int i = 0; i < bits.Length; i++) if (bits[i]) r |= 1 << (bits.Length - i);
+            return r;
+        }
         public double GetReading()
         {
             initializeDevice();
+            double result = 1337;
             if (device != null)
-                return (device.Read() - offset) * calibrationConstant;
-            else return 1337;
+            {
+                result = (device.Read() - offset) * calibrationConstant;
+            }
+            releaseDevice();
+            return result;
         }
         public void Tare()
         {
