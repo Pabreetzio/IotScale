482de8f021a0d36695e626a88710f3ca3154133a
Patrick Graham <Pabreetzio@gmail.com>
2016-03-10 01:42:41 -0500

    Adding a web application project, with hopes of being able to connect it to the scale soon.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace WebApplication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        MqttClient client;
+        public ActionResult Index()
+        {
+            client = new MqttClient("192.168.1.145");
+            var topics = new Dictionary<string, byte>();
+            string thisMachineId = System.Environment.MachineName;
+            client.Connect(thisMachineId);
+            topics.Add("perceive/weight", MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE);
+            client.Subscribe(topics.Keys.ToArray(), topics.Values.ToArray());
+            return View();
+        }
+        public JsonResult ReadWeight()
+        {
+            client.Publish("act/weigh",null);
+            return Json(19);
+        }
+    }
+}
